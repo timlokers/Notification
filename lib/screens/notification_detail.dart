9f758515416e04a1c1398727dfa696f7c4fb05ba
timlokers <39838876+timlokers@users.noreply.github.com>
2019-12-18 15:18:05 +0100

    new code

---
+++ lib/screens/notification_detail.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NotificationDetail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
